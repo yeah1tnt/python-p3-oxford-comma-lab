c5041e0bfd8fe430629d1d48102dc35ed11b23cf
Tong Tang <pichip22@hotmail.com>
2023-07-09 01:44:45 -0400

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
+import ipdb
+
 def oxford_comma(items):
-    return None
+    result = ""
+    if len(items) == 1:
+        result = items[0]
+    elif len(items) == 2:
+        result = items[0] + " and " + items[1]
+    else:
+        result = ", ".join(items[:-1]) + ", and " + items[-1]
+    
+    return result
